be80287f22f4e5fa8c4e76ccc24596b31ddb7dd0
Jeremiah Buddenhagen <git@bitspill.net>
2020-01-08 14:05:01 -0800

    shellcheck

--- build_proto.sh
+++ build_proto.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 echo "Building oip proto files"
-pushd ./src/oip
-protoc --go_out=$GOPATH/src multipart.proto pubkey.proto signedMessage.proto txid.proto
-popd
+pushd ./src/oip || exit
+protoc --go_out="$GOPATH/src" multipart.proto pubkey.proto signedMessage.proto txid.proto
+popd || exit
 
 echo "Building oip5 proto files"
-pushd ./src/oip5
-protoc --go_out=$GOPATH/src -I=. -I=$GOPATH/src/github.com/oipwg/proto/src/oip \
+pushd ./src/oip5 || exit
+protoc --go_out="$GOPATH/src" -I=. -I="$GOPATH/src/github.com/oipwg/proto/src/oip" \
   edit.proto NormalizeRecord.proto NormalizeRecord.proto oip5.proto Record.proto
-popd
+popd || exit
 
 echo "Building oip5 template proto files"
-pushd ./src/oip5/templates
-protoc --go_out=$GOPATH/src  RecordTemplateProto.proto tmpl_433C2783.proto
-popd
+pushd ./src/oip5/templates || exit
+protoc --go_out="$GOPATH/src"  RecordTemplateProto.proto tmpl_433C2783.proto
+popd || exit
 
 echo "Building historian proto files"
-pushd ./src/historian
-protoc --go_out=$GOPATH/src historian.proto
-popd
+pushd ./src/historian || exit
+protoc --go_out="$GOPATH/src" historian.proto
+popd || exit
