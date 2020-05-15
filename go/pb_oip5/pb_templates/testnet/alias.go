d83f59eecf3a54ecdc267bc3a6a0802b96798a9e
Jeremiah Buddenhagen <git@bitspill.net>
2020-05-15 13:28:15 -0700

    add tmpl aliases

---
+++ go/pb_oip5/pb_templates/testnet/alias.go
@@ -0,0 +1,13 @@
+package testnet
+
+import (
+	"github.com/jhump/protoreflect/dynamic"
+)
+
+type RegisteredPublisher = dynamic.Message
+
+type Coin = dynamic.Message
+type SimpleCoinSale = dynamic.Message
+type CommercialContent = dynamic.Message
+
+type Song = dynamic.Message
